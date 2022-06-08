88b4cb63dcfa6ca28f8fde2573fe3660bd250cc3
Munikiran Narpareddi <narmuni123@gmail.com>
2022-06-08 19:34:54 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/munikiran/bmiCalc/bmi_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.munikiran.bmiCalc.bmi_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/input_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class InputScreen extends StatefulWidget {
+  const InputScreen({Key? key}) : super(key: key);
+
+  @override
+  _InputScreenState createState() => _InputScreenState();
+}
+
+class _InputScreenState extends State<InputScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
