2dbb0d5744701c0b94d6d74fba977dcd3cc1faed
Munikiran Narpareddi <narmuni123@gmail.com>
2022-06-13 17:15:02 +0530

    added a result screen

---
+++ lib/screens/result_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class ResultBmi extends StatelessWidget {
+  const ResultBmi({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
