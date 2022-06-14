0a772f7fd10fa2e95930915e9ed5d51889477210
Munikiran Narpareddi <narmuni123@gmail.com>
2022-06-14 08:32:06 +0530

    added the result page text constant and style

--- lib/reusable/style.dart
+++ lib/reusable/style.dart
@@ -14,5 +14,28 @@ const kNumberTextStyle = TextStyle(
 const kLargeButtonTextStyle = TextStyle(
   fontSize: 25.0,
   color: Colors.white,
-  fontWeight: FontWeight.w900,
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+  fontSize: 22.0,
+  color: Color(0xFF24D676),
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyResultTextStyle = TextStyle(
+  fontSize: 22.0,
+  color: Colors.white,
 );
--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,5 +1,6 @@
+import 'package:bmi_calculator/reusable/color_constant.dart';
+import 'package:bmi_calculator/reusable/style.dart';
 import 'package:flutter/material.dart';
-
 
 class ResultBmi extends StatelessWidget {
   const ResultBmi({Key? key}) : super(key: key);
@@ -11,7 +12,61 @@ class ResultBmi extends StatelessWidget {
         title: Text("BMI CALCULATOR"),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Text(
+              "Your Result",
+              style: kTitleTextStyle,
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: boxContainer(
+              color: ColorConstant.kActiveContainerColor,
+              onPress: () {},
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "Noraml",
+                    style: kResultTextStyle,
+                  ),
+                  Text(
+                    "18.3",
+                    style: kBMITextStyle,
+                  ),
+                  Text(
+                    "",
+                    textAlign: TextAlign.center,
+                    style: kBodyResultTextStyle,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget boxContainer(
+      {required Color color,
+      required Widget cardChild,
+      required VoidCallback onPress}) {
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        margin: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: cardChild,
+      ),
     );
   }
 }
